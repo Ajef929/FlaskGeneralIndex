e51c6c05ad15410126b47db4fd8bfac4632f759f
Xiaotian Yang <xya34@uclive.ac.nz>
2023-01-23 09:57:57 +1300

    added counts to the plot on the webpage. Added the Exprt to CSV feature

--- app.py
+++ app.py
@@ -1,43 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from typing import Dict
 from search import search
-import sqlite3
+import pandas as pd
+import os
+
 
 app = Flask(__name__)
 app.secret_key = "#$%#$%^%^BFGBFGBSFGNSGJTNADFHH@#%$%#T#FFWF$^F@$F#$FW"
-
-def init_database():
-	import csv, sqlite3
-
-	con = sqlite3.connect("metadata.db")
-	cur = con.cursor()
-	cur.execute("CREATE TABLE t (dkey, title, doi, author, year);")  # use your column names here
-
-	with open('metadata_test.csv', 'r') as fin:  # `with` statement available in 2.5+
-		# csv.DictReader uses first line in file for column headings by default
-		dr = csv.DictReader(fin)  # comma is default delimiter
-		to_db = [(i['dkey'], i['title'], i['doi'], i['author'], i['year']) for i in dr]
-
-	cur.executemany("INSERT INTO t (dkey, title, doi, author, year) VALUES (?, ?, ?, ?, ?);", to_db)
-	con.commit()
-	con.close()
-
+IMG_FOLDER = os.path.join('static', 'IMG')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-
 @app.route("/search", methods=["POST", "GET"])
-def searchr():
+def searcht():
 	if request.method == "POST":
 		query = request.form["query"]
 		search_within = request.values.get("type")
 		start_year = request.values.get("start")
 		end_year = request.values.get("end")
-		results, info = search(query, search_within, start_year, end_year)
-	return render_template("search.html", results=results, info=info, query=query)
-
+		export = request.values.get("export_check")
+		results, info = search(query, search_within, start_year, end_year, export)
+		year_plot = os.path.join(app.config['UPLOAD_FOLDER'], 'year_plot.png')
+	return render_template("search.html", results=results, info=info, query=query,total_results=len(results),year_plot=year_plot)
 
 if __name__ == '__main__':
-	# init_database()
 	app.run(debug=True, host="132.181.102.8")
--- search.py
+++ search.py
@@ -1,7 +1,11 @@
-import sqlite3
+import psycopg2 as p
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-def search(query, search_within, start_year, end_year):
-    results = {}
+def search(query, search_within, start_year, end_year, export):
+    results = {'dkey':[], 'doi':[], 'title':[], 'author':[], 'year':[]}
     if start_year != 'all_start_year' and end_year != 'all_end_year':
         if start_year > end_year:
             info = "end year must greater than start year"
@@ -14,20 +18,43 @@ def search(query, search_within, start_year, end_year):
         end_year = 2024
     else:
         end_year = int(end_year) + 1
-    con = sqlite3.connect('metadata.db')
+    con = p.connect('postgresql://rccuser:password@localhost:5432/generalindex_metadata')
     cur = con.cursor()
     if search_within == 'title':
-        sql = "select * from t where title like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
+        sql = "select dkey,doi,title,author,year from metadata_recent where title like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     elif search_within == 'author':
-        sql = "select * from t where author like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
+        sql = "select dkey,doi,title,author,year from metadata_recent where author like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     elif search_within == 'doi':
-        sql = "select * from t where doi like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
+        sql = "select dkey,doi,title,author,year from metadata_recent where doi like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     else:
-        sql = "select * from t limit 1000"
-    data = cur.execute(sql)
+        sql = "select dkey,doi,title,author,year from metadata_recent limit 1000;"
+    cur.execute(sql)
+    data = cur.fetchall()
     for item in data:
-        results[item[0]] = [item[2], item[1], item[3], item[4]]
+        results['dkey'].append(item[0])
+        results['doi'].append(item[2])
+        results['title'].append(item[1])
+        results['author'].append(item[3])
+        results['year'].append(item[4])
+    # use pandas dataframe to store results for later use, you can use the dataframe to create plot.
+    metadata_df = pd.DataFrame(data=results)
+    # if user ticks export as csv, the result will save in the same directory where they run the app.
+    # Note that only one export can be saved at any one time
+    # filestamp
+    file_to_save = rf'./results_Datetime:{datetime.now()}_SearchTerm:{query}_YearBetween:{start_year}And{end_year}.csv'
+    if export == 'yes':
+        metadata_df.to_csv(file_to_save, sep='\t', encoding='utf-8', header=True, index=False)
+    
     cur.close()
     con.close()
-    info = 'succed'
-    return results, info
+    
+    #plot settings
+    fig, ax = plt.subplots()
+    ax = metadata_df['year'].value_counts(sort=False).plot(ax=ax, kind='bar')
+    ax.bar_label(ax.containers[0])
+    ax.set_ylabel('Frequency')
+    fig.suptitle("Frequency over time")
+    fig.savefig("static/IMG/year_plot.png")
+    
+    info = ''
+    return metadata_df, info
