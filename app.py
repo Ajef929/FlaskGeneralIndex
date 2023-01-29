647ac20218039e72a99ca78c1fcab8fd68a338be
Xiaotian Yang <xya34@uclive.ac.nz>
2023-01-30 10:19:47 +1300

    Revert "Commit before adding button functionality"
    This reverts commit 9d6e9e8e527ff1bfd93f976ac48122e90f5be985.

--- app.py
+++ app.py
@@ -26,18 +26,5 @@ def searcht():
 		year_plot = os.path.join(app.config['UPLOAD_FOLDER'], 'year_plot.png')
 	return render_template("search.html", results=results, info=info, query=query,total_results=len(results),year_plot=year_plot)
 
-
-@app.route("/download_file")
-def download_file():
-    # Code to generate the file content
-    content = obtainResults()
-    # Set the content type and header to indicate a file download
-    response = make_response(content)
-    response.headers["Content-Disposition"] = "attachment; filename=sample.txt"
-    response.headers["Content-Type"] = "text/plain"
-    return response
-
-
-
 if __name__ == '__main__':
 	app.run(debug=True, host="132.181.102.8")
