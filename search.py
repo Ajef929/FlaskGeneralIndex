9cad0eff3d39f7e8ef5b7ca7fce3682f4a70523f
Xiaotian Yang <xya34@uclive.ac.nz>
2023-02-01 22:44:46 +1300

    add lowercase column for title, author, journal, increase a little running time.

--- search.py
+++ search.py
@@ -4,8 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pydoi
+import time
 
 def search(query, search_within, start_year, end_year, export):
+    start_time = time.time()
     results = {'dkey':[], 'doi':[], 'title':[], 'author':[], 'year':[],'journal':[], 'link':[]}
     if start_year != 'all_start_year' and end_year != 'all_end_year':
         if start_year > end_year:
@@ -23,18 +25,21 @@ def search(query, search_within, start_year, end_year, export):
     cur = con.cursor()
     query = query.lower()
     if search_within == 'title':
-        sql = "select dkey,doi,title,author,year,journal from metadata_recent where LOWER(title) like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
+        sql = "select dkey,doi,title,author,year,journal from metadata_recent where title_lwr like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     elif search_within == 'author':
         sql = "select dkey,doi,title,author,year,journal from metadata_recent where LOWER(author) like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     elif search_within == 'doi':
         sql = "select dkey,doi,title,author,year,journal from metadata_recent where doi like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     elif search_within == "journal":
-        sql = "select dkey,doi,title,author,year,journal from metadata_recent where LOWER(journal) like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
+        sql = "select dkey,doi,title,author,year,journal from metadata_recent where journal like '%{query}%' and year > '{start_year}' and year < '{end_year}' limit 10000".format(query=query, start_year=start_year, end_year=end_year)
     else:
         sql = "select dkey,doi,title,author,year,journal from metadata_recent limit 1000;"
     cur.execute(sql)
     data = cur.fetchall()
     i = 0
+    end_time = time.time()
+    print(end_time - start_time)
+    start_time = time.time()
     for item in data:
         results['dkey'].append(item[0])
         results['doi'].append(item[1])
@@ -42,12 +47,14 @@ def search(query, search_within, start_year, end_year, export):
         results['author'].append(item[3])
         results['year'].append(item[4])
         results['journal'].append(item[5])
-        if i < 40:
+        if i < 100:
             results['link'].append(pydoi.get_url(item[1]))
         else:
             results['link'].append('None')
         i += 1
     # use pandas dataframe to store results for later use, you can use the dataframe to create plot.
+    end_time = time.time()
+    print(end_time - start_time)
     metadata_df = pd.DataFrame(data=results)
     # if user ticks export as csv, the result will save in the same directory where they run the app.
     # Note that only one export can be saved at any one time
